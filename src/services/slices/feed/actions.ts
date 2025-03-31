33f5b01c8fd06ca63c18ab10a067c0374b0f31c7
Vas Tregubov <vastregubov@gmail.com>
2025-03-31 12:31:00 +0300

    refactor(services/feed/actions): отрефакторен код

--- src/services/slices/feed/actions.ts
+++ src/services/slices/feed/actions.ts
@@ -2,11 +2,9 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { getFeedsApi, getOrderByNumberApi } from '../../../utils/burger-api';
 
-export const getFeedsThunk = createAsyncThunk('feeds/getFeeds', async () =>
-  getFeedsApi()
-);
+export const getFeedsThunk = createAsyncThunk('feeds/getFeeds', getFeedsApi);
 
 export const getOrderByNumberThunk = createAsyncThunk(
   'orders/getOrder',
-  async (number: number) => getOrderByNumberApi(number)
+  getOrderByNumberApi
 );
